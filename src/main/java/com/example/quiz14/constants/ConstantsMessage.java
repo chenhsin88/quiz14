37347f040975df13badfe74dbbc784ba2156ae53
Hsin <mc881221@gmail.com>
2025-05-23 10:07:49 +0800

    first commit

---
+++ src/main/java/com/example/quiz14/constants/ConstantsMessage.java
@@ -0,0 +1,35 @@
+package com.example.quiz14.constants;
+
+public class ConstantsMessage {
+
+	public static final String PARAM_TITLE_ERROR = "param title error!!";
+	
+	public static final String PARAM_DESCRIPTION_ERROR = "param description error!!";
+	
+	public static final String PARAM_START_DATE_ERROR = "param start date error!!";
+	
+	public static final String PARAM_END_DATE_ERROR = "param end date error!!";
+	
+	public static final String PARAM_QUES_ID_ERROR = "param ques_id error!!";
+	
+	public static final String PARAM_QUIZ_ID_ERROR = "param quiz_id error!!";
+	
+	public static final String PARAM_QUESTION_ERROR = "param question error!!";
+	
+	public static final String PARAM_QUES_TYPE_ERROR = "param ques type error!!";
+	
+	public static final String PARAM_OPTIONS_ERROR = "param options error!!";
+	
+	public static final String USER_NAME_IS_NECESSARY = "User name is necessary!!";
+	
+	public static final String PHONE_IS_NECESSARY = "Phone is necessary!!";
+	
+	public static final String EMAIL_IS_NECESSARY = "Email is necessary!!";
+	
+	public static final String ANSWER_IS_NECESSARY = "Answer is necessary!!";
+	
+	public static final String USER_AGE_ERROR = "User age error!!";
+	
+	
+	
+}
